802300d569684c7e40f707a6138deb06bdb191a7
grinvald89 <grinvald89@ya.ru>
2024-03-18 20:45:40 +0300

    Updated root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { ChartModule, ListModule } from '../modules';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    ChartModule,
+    ListModule,
+    MatTabsModule,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'test-bur';
-}
+export class AppComponent { }
